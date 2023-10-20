caa4794774195d00e6ff503b6aacd15b058624dc
Slava Petrov <slavapmk@gmail.com>
2023-10-20 22:47:54 +0300

    Fixed translator unknown host exception

--- src/main/kotlin/ru/slavapmk/ignat/Translator.kt
+++ src/main/kotlin/ru/slavapmk/ignat/Translator.kt
@@ -7,6 +7,8 @@ import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import ru.slavapmk.ignat.io.YandexTranslateApi
 import ru.slavapmk.ignat.io.yandex.TranslateRequest
+import ru.slavapmk.ignat.io.yandex.TranslateResponse
+import java.net.UnknownHostException
 
 class Translator(private val debugMode: Boolean) {
     private val api = Retrofit
@@ -30,15 +32,22 @@ class Translator(private val debugMode: Boolean) {
         .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
         .build().create(YandexTranslateApi::class.java)
 
-    fun translate(request: String, target: String, token: String, folder: String) =
-        api.request(
-            "Bearer $token",
-            TranslateRequest(
-                folder,
-                arrayOf(request),
-                target
-            )
-        ).blockingSingle()
+    fun translate(request: String, target: String, token: String, folder: String): TranslateResponse {
+        while (true) {
+            try {
+                return api.request(
+                    "Bearer $token",
+                    TranslateRequest(
+                        folder,
+                        arrayOf(request),
+                        target
+                    )
+                ).blockingSingle()
+
+            } catch (_: UnknownHostException) {
+            }
+        }
+    }
 }
 
 
